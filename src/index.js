f2daf0069641ea32685c4d5aacecab9770e92bb3
Floris Veldhuizen <floris.veldhuizen@nedap.com>
2022-01-21 14:05:06 +0100

    push it through the limit

--- src/index.js
+++ src/index.js
@@ -1,31 +1,41 @@
 import Phaser from 'phaser';
 import logoImg from './assets/logo.png';
+import skyImg from './assets/sky.png';
+import starImg from './assets/star.png';
 
-class MyGame extends Phaser.Scene
+function preload ()
 {
-  constructor ()
-  {
-    super();
-  }
+  this.load.image('logo', logoImg);
+  this.load.image('sky', skyImg);
+  this.load.image('ground', 'assets/platform.png');
+  this.load.image('star', starImg);
+  this.load.image('bomb', 'assets/bomb.png');
+  this.load.spritesheet('dude',
+    'assets/dude.png',
+    { frameWidth: 32, frameHeight: 48 }
+  );
+}
 
-  preload ()
-  {
-    this.load.image('logo', logoImg);
-  }
+function create ()
+{
+  this.add.image(400, 300, 'sky');
+  this.add.image(400, 300, 'star');
+  const logo = this.add.image(400, 150, 'logo');
 
-  create ()
-  {
-    const logo = this.add.image(400, 150, 'logo');
+  this.tweens.add({
+    targets: logo,
+    y: 450,
+    duration: 2000,
+    ease: "Power2",
+    yoyo: true,
+    loop: -1
+  });
 
-    this.tweens.add({
-      targets: logo,
-      y: 450,
-      duration: 2000,
-      ease: "Power2",
-      yoyo: true,
-      loop: -1
-    });
-  }
+}
+
+function update ()
+{
+
 }
 
 const config = {
@@ -33,7 +43,11 @@ const config = {
   parent: 'phaser-example',
   width: 800,
   height: 600,
-  scene: MyGame
+  scene: {
+    preload: preload,
+    create: create,
+    update: update
+  }
 };
 
 const game = new Phaser.Game(config);
