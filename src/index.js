c96748d6ae545cd53aff54c489c6567a8ce8acff
Floris Veldhuizen <floris.veldhuizen@nedap.com>
2022-02-06 12:50:04 +0100

    split file

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 import Phaser from "phaser";
+import makeAnimations from "./helpers/animations";
+import Bullet from "./sprites/Bullet";
+import "./index.css";
+
+// IMAGE IMPORT
 import skyImg from "./assets/sky.png";
 import starImg from "./assets/star.png";
 import platformImg from "./assets/platform.png";
@@ -7,7 +12,6 @@ import dudeImg from "./assets/dude.png";
 import bulletImg from "./assets/bullet.png";
 import diamondImg from "./assets/diamond.png"
 import flockyImg from "./assets/flocky.png";
-import "./index.css";
 
 // Tutorial:
 // https://phaser.io/tutorials/making-your-first-phaser-3-game/part3
@@ -91,25 +95,7 @@ function create() {
   player.setMaxVelocity(maxVelocity);
   player.setCollideWorldBounds(true);
 
-  this.anims.create({
-    key: "left",
-    frames: this.anims.generateFrameNumbers("dude", { start: 0, end: 3 }),
-    frameRate: 10,
-    repeat: -1,
-  });
-
-  this.anims.create({
-    key: "turn",
-    frames: [{ key: "dude", frame: 4 }],
-    frameRate: 20,
-  });
-
-  this.anims.create({
-    key: "right",
-    frames: this.anims.generateFrameNumbers("dude", { start: 5, end: 8 }),
-    frameRate: 10,
-    repeat: -1,
-  });
+  makeAnimations(this);
 
   // STARS
   stars = this.physics.add.group({
@@ -204,11 +190,6 @@ function collectDiamond(player, diamond) {
   diamond.disableBody(true, true);
   score += 50;
   scoreText.setText("Score: " + score);
-}
-
-function enemyDrops(enemy) {
-  enemy.disableBody(true, true);
-
 }
 
 function hitBomb(player, bomb) {
@@ -309,48 +290,3 @@ const flashColor = (object, color) => {
     callbackScope: this,
   });
 }
-
-const Bullet = new Phaser.Class({
-  Extends: Phaser.GameObjects.Image,
-
-  initialize:
-    // Bullet Constructor
-    function Bullet(scene) {
-      Phaser.GameObjects.Image.call(this, scene, 0, 0, "bullet");
-      this.speed = 1;
-      this.born = 0;
-      this.direction = 0;
-      this.xSpeed = 0;
-      this.ySpeed = 0;
-      this.setSize(12, 12, true);
-    },
-
-  // Fires a bullet from the player to the reticle
-  fire: function (shooter, target) {
-    this.setPosition(shooter.x, shooter.y); // Initial position
-    this.direction = Math.atan((target.x - this.x) / (target.y - this.y));
-
-    // Calculate X and y velocity of bullet to moves it from shooter to target
-    if (target.y >= this.y) {
-      this.xSpeed = this.speed * Math.sin(this.direction);
-      this.ySpeed = this.speed * Math.cos(this.direction);
-    } else {
-      this.xSpeed = -this.speed * Math.sin(this.direction);
-      this.ySpeed = -this.speed * Math.cos(this.direction);
-    }
-
-    this.rotation = shooter.rotation; // angle bullet with shooters rotation
-    this.born = 0; // Time since new bullet spawned
-  },
-
-  // Updates the position of the bullet each cycle
-  update: function (time, delta) {
-    this.x += this.xSpeed * delta;
-    this.y += this.ySpeed * delta;
-    this.born += delta;
-    if (this.born > 1800) {
-      this.setActive(false);
-      this.setVisible(false);
-    }
-  },
-});
