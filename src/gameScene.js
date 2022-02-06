64409a1f8224b2d3b35ad751dd866fdd7bf23da8
wouterkg <wouterkaag@gmail.com>
2022-02-06 13:43:51 +0100

    Merge fixes

--- src/gameScene.js
+++ src/gameScene.js
@@ -7,6 +7,8 @@ import dudeImg from "./assets/dude.png";
 import bulletImg from "./assets/bullet.png";
 import diamondImg from "./assets/diamond.png";
 import flockyImg from "./assets/flocky.png";
+import makeAnimations from "./helpers/animations";
+import Bullet from "./sprites/Bullet";
 
 // CONSTANTS
 const accelerationSpeed = 75;
@@ -70,25 +72,7 @@ class GameScene extends Phaser.Scene {
     this.player.setMaxVelocity(maxVelocity);
     this.player.setCollideWorldBounds(true);
 
-    this.anims.create({
-      key: "left",
-      frames: this.anims.generateFrameNumbers("dude", { start: 0, end: 3 }),
-      frameRate: 10,
-      repeat: -1,
-    });
-
-    this.anims.create({
-      key: "turn",
-      frames: [{ key: "dude", frame: 4 }],
-      frameRate: 20,
-    });
-
-    this.anims.create({
-      key: "right",
-      frames: this.anims.generateFrameNumbers("dude", { start: 5, end: 8 }),
-      frameRate: 10,
-      repeat: -1,
-    });
+    makeAnimations(this);
 
     // STARS
     this.stars = this.physics.add.group({
@@ -200,8 +184,8 @@ class GameScene extends Phaser.Scene {
   collectStar(player, star) {
     star.disableBody(true, true);
 
-    score += 10;
-    this.scoreText.setText("Score: " + score);
+    this.score += 10;
+    this.scoreText.setText("Score: " + this.score);
 
     if (this.stars.countActive(true) === 0) {
       this.stars.children.iterate(function (child) {
@@ -228,12 +212,8 @@ class GameScene extends Phaser.Scene {
 
   collectDiamond(player, diamond) {
     diamond.disableBody(true, true);
-    score += 50;
-    this.scoreText.setText("Score: " + score);
-  }
-
-  enemyDrops(enemy) {
-    enemy.disableBody(true, true);
+    this.score += 50;
+    this.scoreText.setText("Score: " + this.score);
   }
 
   hitBomb(player, bomb) {
@@ -338,48 +318,3 @@ class GameScene extends Phaser.Scene {
 }
 
 export default GameScene;
-
-const Bullet = new Phaser.Class({
-  Extends: Phaser.GameObjects.Image,
-
-  initialize:
-    // Bullet Constructor
-    function Bullet(scene) {
-      Phaser.GameObjects.Image.call(this, scene, 0, 0, "bullet");
-      this.speed = 1;
-      this.born = 0;
-      this.direction = 0;
-      this.xSpeed = 0;
-      this.ySpeed = 0;
-      this.setSize(12, 12, true);
-    },
-
-  // Fires a bullet from the player to the reticle
-  fire: function (shooter, target) {
-    this.setPosition(shooter.x, shooter.y); // Initial position
-    this.direction = Math.atan((target.x - this.x) / (target.y - this.y));
-
-    // Calculate X and y velocity of bullet to moves it from shooter to target
-    if (target.y >= this.y) {
-      this.xSpeed = this.speed * Math.sin(this.direction);
-      this.ySpeed = this.speed * Math.cos(this.direction);
-    } else {
-      this.xSpeed = -this.speed * Math.sin(this.direction);
-      this.ySpeed = -this.speed * Math.cos(this.direction);
-    }
-
-    this.rotation = shooter.rotation; // angle bullet with shooters rotation
-    this.born = 0; // Time since new bullet spawned
-  },
-
-  // Updates the position of the bullet each cycle
-  update: function (time, delta) {
-    this.x += this.xSpeed * delta;
-    this.y += this.ySpeed * delta;
-    this.born += delta;
-    if (this.born > 1800) {
-      this.setActive(false);
-      this.setVisible(false);
-    }
-  },
-});
